59bd753b6a55a38907e8b127e01b5f7bbf36295c
rushuiguan <rushui@gmail.com>
2025-09-04 22:40:23 -0400

    working on a build script

--- import-script.ps1
+++ import-script.ps1
@@ -1,5 +1,15 @@
-Invoke-WebRequest `
-	-Uri https://raw.githubusercontent.com/RushuiGuan/dev-scripts/refs/heads/main/dev-scripts.psm1 `
-	-OutFile $PSScriptRoot/dev-scripts.psm1
+$branch = "dev"
 
-Import-Module $PSScriptRoot/dev-scripts.psm1
+if (Test-Path $PSScriptRoot/dev-scripts -PathType Container) {
+	Get-Item $PSScriptRoot/dev-scripts | Remove-Item -Recurse -Force
+}
+
+Invoke-WebRequest `
+	-Uri https://github.com/RushuiGuan/dev-scripts/archive/refs/heads/$branch.zip `
+	-OutFile $PSScriptRoot/dev-scripts.zip
+
+Expand-Archive -Path $PSScriptRoot/dev-scripts.zip -DestinationPath $PSScriptRoot -Force
+Remove-Item -Path $PSScriptRoot/dev-scripts.zip
+
+Move-Item -Path $PSScriptRoot/dev-scripts-$branch -Destination $PSScriptRoot/dev-scripts
+Import-Module $PSScriptRoot/dev-scripts/dev-scripts.psm1
